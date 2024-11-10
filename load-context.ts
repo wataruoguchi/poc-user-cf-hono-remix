a09d6f2c747a59b7594c7044ac8f81832f632cbf
wataru <wataru.oguchi.0to1@gmail.com>
2024-11-10 15:18:24 -0800

    The template to integrate with `hono-remix-adapter`
    1. pnpm create cloudflare@latest --framework=remix
    2. Integrate it with `hono-remix-adapter`

--- load-context.ts
+++ load-context.ts
@@ -1,4 +1,11 @@
+// https://github.com/yusukebe/hono-remix-adapter/blob/main/example/load-context.ts
+
+import { type AppLoadContext } from "@remix-run/cloudflare";
 import { type PlatformProxy } from "wrangler";
+
+interface Env {
+  MY_VAR: string;
+}
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
@@ -7,3 +14,15 @@ declare module "@remix-run/cloudflare" {
     cloudflare: Cloudflare;
   }
 }
+type GetLoadContext = (args: {
+  request: Request;
+  context: { cloudflare: Cloudflare };
+}) => AppLoadContext;
+
+// Shared implementation compatible with Vite, Wrangler, and Cloudflare Pages
+export const getLoadContext: GetLoadContext = ({ context }) => {
+  return {
+    ...context,
+    extra: 'stuff'
+  };
+};
